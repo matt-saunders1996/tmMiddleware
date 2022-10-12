8e3972a7df425f930c9142c91889633f8cc2b4e4
root <root@DESKTOP-MBHM5G2.localdomain>
2022-10-12 21:23:14 +0100

    add email addresses from mockapi

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const helloWorld = require('./scheduledFunctions/helloWorld')
 const mailInit = require('./scheduledFunctions/emailScheduler')
 
 const app = express();
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), () => {
     console.log(`listening on port ${app.get("port")}`);
--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,6 +1,34 @@
 const mailer = require('nodemailer');
+const axios = require("axios");
 
-exports.sendMail = () => {
+const getTasks = async () => {
+    const tasks = await axios.get("https://6341338f20f1f9d7996dfc67.mockapi.io/tasks");
+    console.log(`tasks:${JSON.stringify(tasks.data)}`);
+    return tasks;
+}
+
+const convertToTimestamp = (date) => {
+    const dateObj = new Date(date);
+
+    return Math.floor(dateObj.getTime() / 1000)
+}
+
+exports.sendMail = async () => {
+    const {data: tasks} = await getTasks();
+    console.log(tasks);
+
+    const emailAddresses = [];
+    tasks.map(task => {
+        if(task.date){
+            const timestamp = convertToTimestamp(task.date);
+
+            if (timestamp <= Date.now() && task.email) {
+                emailAddresses.push(task.email);
+            }
+        }
+
+    })
+
     const transport = mailer.createTransport({
         host: "smtp.mailtrap.io",
         port: 2525,
@@ -11,10 +39,10 @@ exports.sendMail = () => {
     });
     const mailData = {
         from: 'taskmanager@gmail.com',  // sender address
-        to: 'matt.jsaunds@outlook.com, matt.jsaunds@gmail.com',   // list of receivers
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!',
-        html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>'
+        to: emailAddresses,  // list of receivers
+        subject: 'overdue task',
+        text: 'your task is overdue',
+        html: '<b>Hey there! </b><br> you have an overdue task with taskmaster!<br/>'
     };
 
     transport.sendMail(mailData, function (err, info) {
